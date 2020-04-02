0bd30d89946bf8be11bc9545a9ffc6d226c7a0cc
James Pettit <james@academia.edu>
2020-04-02 16:46:10 +0800

    Playback and staves!

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
 
+import { FaPlay } from 'react-icons/fa';
 import update from 'immutability-helper';
 import styled from 'styled-components'
-import { Synth, PolySynth } from 'tone';
+import { Synth, PolySynth, Part, Transport } from 'tone';
 
 const Container = styled.div`
   width: 100%;
@@ -48,6 +49,49 @@ const Blackkey = styled.div<ButtonProps>`
   left: 68%;
 `;
 
+const Staff = styled.div`
+  width: 80%;
+  display: flex;
+  flex-direction: row;
+  margin: 16px;
+`
+
+const Bar = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+`
+
+const Beat = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const Line = styled.div`
+  height: 2px;
+  background: black;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+
+const Space = styled.div`
+  height: 12px;
+  background: white;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+
+const Note = styled.div`
+  height: 10px;
+  width: 10px;
+  background: black;
+  border-radius: 5px;
+  margin: 0 5px;
+  z-index: 1;
+`
+
 interface State {
   keyPressed: { [key: string]: boolean }
   history: Array<string>
@@ -56,6 +100,7 @@ interface State {
 export default class App extends React.Component<{}, State> {
 
   synth: PolySynth = new PolySynth({ maxPolyphony: 10, voice: Synth }).toDestination()
+  playback?: Part
   notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
   keybindings = {
     4: "zxcvbnm",
@@ -122,6 +167,18 @@ export default class App extends React.Component<{}, State> {
     }
   }
 
+  onPlay() {
+    if (this.playback) {
+      this.playback.stop();
+      Transport.stop();
+    }
+    Transport.start();
+    this.playback = new Part((time, event) => {
+      this.synth.triggerAttackRelease(event.note, event.dur, time);
+    }, this.state.history.map((note, i) => { return { time: i, note: note, dur: '4n' }; }));
+    this.playback.start(0);
+  }
+
   render() {
     const octaves = [
       this.notes.map(note => note + 4),
@@ -130,9 +187,29 @@ export default class App extends React.Component<{}, State> {
     ];
     return (
       <Container onKeyDown={this.onKeyDown.bind(this)} onKeyUp={this.onKeyUp.bind(this)} tabIndex={-1}>
-        <p>
-          {this.state.history.join(' ')}
-        </p>
+        <div>
+          <FaPlay onClick={this.onPlay.bind(this)} />
+        </div>
+        <Staff>
+          <Bar>
+            {this.state.history.map((note, i) => <Beat key={i}>
+              <Line>{note === 'A5' && <Note />}</Line>
+              <Space>{note === 'G5' && <Note />}</Space>
+              <Line>{note === 'F5' && <Note />}</Line>
+              <Space>{note === 'E5' && <Note />}</Space>
+              <Line>{note === 'D5' && <Note />}</Line>
+              <Space>{note === 'C5' && <Note />}</Space>
+              <Line>{note === 'B4' && <Note />}</Line>
+              <Space>{note === 'A4' && <Note />}</Space>
+              <Line>{note === 'G4' && <Note />}</Line>
+              <Space>{note === 'F4' && <Note />}</Space>
+              <Line>{note === 'E4' && <Note />}</Line>
+              <Space>{note === 'D4' && <Note />}</Space>
+              <Line>{note === 'C4' && <Note />}</Line>
+            </Beat>
+            )}
+          </Bar>
+        </Staff>
         <Keyboard>
           {octaves.map((notes, i) => <Octave key={i}>
             {notes.map((note, i) => {
